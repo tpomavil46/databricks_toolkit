2fe570dd05f5acf1f8a2e8ef17d296a87f17abd5
Tim Pomaville <tpomavil@icloud.com>
2025-04-01 13:40:23 -0700

    adding ci.yml

--- jobs/ingest_customer.py
+++ jobs/ingest_customer.py
@@ -2,4 +2,5 @@ from utils.io import load_csv_to_df, write_df_as_table
 
 def run(spark, input_path: str, output_table: str):
     df = load_csv_to_df(spark, input_path)
-    write_df_as_table(df, output_table)
+    df.show()
+    df.write.mode("overwrite").saveAsTable("customers_cleaned")
